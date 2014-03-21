1d85eca7b146f13555eddbe6a1138f475fa3f6cf
Kevin Clark <kevin@kevinclark.ca>
2014-03-21 19:38:20 -0400

    Initial Commit

---
+++ Expense/KEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KEVAppDelegate.h
+//  Expense
+//
+//  Created by Kevin Clark on 2014-03-21.
+//  Copyright (c) 2014 Kevin Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Expense/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Expense
+//
+//  Created by Kevin Clark on 2014-03-21.
+//  Copyright (c) 2014 Kevin Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KEVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KEVAppDelegate class]));
+    }
+}
